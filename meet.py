c879484a83306e21196cc6d49ff7411bb330cb89
Erfan Moharamzadeh <e.moharamzadeh1@gmail.com>
2023-02-03 20:55:23 +0330

    write meet.py

---
+++ meet.py
@@ -0,0 +1,6 @@
+class Meet:
+	def __init__(self, title, records: list, startDate=None, endDate=None):
+		self.title = title
+		self.records = records
+		self.startDate = startDate
+		self.endDate = endDate
