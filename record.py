3aed709e2035dddb1a69fb6f6d790740c158716d
Erfan Moharamzadeh <e.moharamzadeh1@gmail.com>
2023-02-04 18:08:08 +0330

    Big change record.py: add status, __setitem__, __call__, __bool__

--- record.py
+++ record.py
@@ -1,6 +1,20 @@
+import man
+
 class Record:
-	def __init__(self, title, buyer, manDebt: dict, dateTime=None):
+	def __init__(self, title, buyer: man.Man, dateTime=None):
 		self.title = title
 		self.buyer = buyer
-		self.manDebt = manDebt
 		self.dateTime = dateTime
+		self.status = 'unpaid'
+		self.manDebt = dict()
+
+	def __setitem__(self, man: man.Man, amount):
+		self.manDebt[man] = amount
+
+	def __call__(self, status: str):
+		self.status = status
+
+	def __bool__(self):
+		if self.status == 'paid':
+			return True
+		return False
