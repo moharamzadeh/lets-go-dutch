e1ef6f623771dc5969044585be66e28384b0752a
Erfan Moharamzadeh <e.moharamzadeh1@gmail.com>
2023-02-04 18:23:07 +0330

    write __call__, __bool__, @property Meet.mans

--- meet.py
+++ meet.py
@@ -1,7 +1,25 @@
+import record
+
 class Meet:
 	def __init__(self, title, startDate=None, endDate=None):
 		self.title = title
 		self.startDate = startDate
 		self.endDate = endDate
 		self.records = list()
-		self.mans = None
+
+	def __call__(self, record: record.Record):
+		self.records.append(record)
+
+	def __bool__(self):
+		for record in self.records:
+			if not record:
+				return False
+		return True
+
+	@property
+	def mans(self):
+		allMan = set()
+		for record in self.records:
+			allMan.add(record.buyer)
+			allMan.add(record.mans)
+		return allMan
