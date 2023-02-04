9aa8b24a22b35d7364463da86005fb5f6ad661b2
Erfan Moharamzadeh <e.moharamzadeh1@gmail.com>
2023-02-04 21:02:42 +0330

    FIX: debt debtor and creditor change

--- debt.py
+++ debt.py
@@ -3,10 +3,10 @@ import record
 class Debt(record.Record):
 	def __init__(self, title, debtor, creditor, amount, dateTime=None):
 		self.title = title
-		self.buyer = debtor
+		self.buyer = creditor
 		self.dateTime = dateTime
 		self.manDebt = dict()
-		self.manDebt[creditor] = amount
+		self.manDebt[debtor] = amount
 
 	def __setitem__(self):
 		pass
