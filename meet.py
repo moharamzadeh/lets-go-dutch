1ba6aabd10d596c94b98c1f34b0a62d8bd1f67fb
Erfan Moharamzadeh <e.moharamzadeh1@gmail.com>
2023-02-04 18:09:44 +0330

    delete parameter records in Meet.__init__

--- meet.py
+++ meet.py
@@ -1,8 +1,7 @@
 class Meet:
-	def __init__(self, title, records: list, startDate=None, endDate=None):
+	def __init__(self, title, startDate=None, endDate=None):
 		self.title = title
-		self.records = records
 		self.startDate = startDate
 		self.endDate = endDate
-
+		self.records = list()
 		self.mans = None
