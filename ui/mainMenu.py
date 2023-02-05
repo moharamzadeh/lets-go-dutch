e17b4b352a30c8accebfa7b26d5a1872cd2d9849
Erfan Moharamzadeh <e.moharamzadeh1@gmail.com>
2023-02-06 02:23:37 +0330

    write mainMenu.py

---
+++ ui/mainMenu.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class Window():
+	def __init__(self):
+		app = QApplication(sys.argv)
+
+		self.__createWidget()
+		self.__createLayout()
+
+		sys.exit(app.exec_())
+
+	def __createWidget(self):
+		self.window = QMainWindow()
+		self.window.setWindowTitle('محاسبه دنگ')
+		self.window.showMaximized()
+		self.window.show()
+
+	def __createLayout(self):
+		self.__createMenuBar()
+	
+	def __createMenuBar(self):
+		menu = self.window.menuBar()
+		file = menu.addMenu('File')
+		btnNew = file.addAction('New')
+		file.addSeparator()
+		btnExit = file.addAction('Exit')
+		btnExit.triggered.connect(exit)
+
+		other = menu.addMenu('Help')
+		about = other.addAction('About')
+
+
+if __name__ == '__main__':
+	window = Window()
