5daeab61603b9d054092732951c4b3fb9ee62f59
Erfan Moharamzadeh <e.moharamzadeh1@gmail.com>
2023-02-07 11:49:36 +0330

    return 0 for cost Debt

--- debt.py
+++ debt.py
@@ -3,4 +3,9 @@ import record
 class Debt(record.Record):
 	def __init__(self, title, debtor, creditor, amount, dateTime=None):
 		super().__init__(title, creditor, dateTime)
+		self.amount = amount
 		self.userDebt[debtor] = amount
+	
+	@property
+	def cost(self):
+		return 0
